dd78984e0215a82d79d9101c9be58631bda4904a
sayqz <i@sayqz.com>
2026-06-06 16:11:57 +0800

    chore: bump version to 0.1.2

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   static const appName = '新理Lite';
-  static const appVersion = '0.1.1';
+  static const appVersion = '0.1.2';
   static const apiBaseUrl = 'https://xllite.sayqz.com';
 }
